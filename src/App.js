fdc58f4444662f5d157651755722a6248b92eee1
h.mertozgen@gmail.com <h.mertozgen@gmail.com>
2022-05-30 22:08:23 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Add from "./components/Add";
+import Header from "./components/Header";
+import Watched from "./components/Watched";
+import WatchList from "./components/WatchList";
 
 function App() {
-  return <div className="App">sas</div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<WatchList />} />
+        <Route path="/watched" element={<Watched />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
